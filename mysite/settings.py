da723236ce2c9a52bb03ed4a5d5d32317ed8f156
tinuolaa00 <tinuolaa00@gmail.com>
2017-07-21 10:46:51 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4p@_)$6rd0=0*xj+kc3s&6uvad^0rqy!4e141mobtye-xra)m!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tinuolaa00.pythonanywhere.com']
 
 
 # Application definition
